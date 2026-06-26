69574889f78982c5bff43eb021318f7c9db27e97
Benoit Denkinger <benoit.denkinger@cern.ch>
2026-06-26 09:27:23 +0200

    docs: fixed editUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/hep-soc/peakrdl-halcpp/tree/master/',
+            'https://github.com/hep-soc/peakrdl-halcpp/tree/master/docs/',
         },
         // blog: {
         //   showReadingTime: true,
